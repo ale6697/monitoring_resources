db8311f75c601ce109afa500aa284039fca95dac
Alessandro De Rossi <alessandro.derossi@lutech.it>
2025-05-06 11:23:40 +0200

    initial commit : monitoring scripts

---
+++ monitoring_resources.py
@@ -0,0 +1,28 @@
+import psutil
+from datetime import datetime
+
+def send_alert(subject , message):
+    print(f"ALERT\nSubject : {subject}\nMessage : {message}")
+
+def check_disk():
+    usage = psutil.disk_usage("/")
+    percent = usage.percent
+    if percent > 80:
+        print("Disk usage over 80%")
+        send_alert(subject="ALERT DISK USAGE",message=f"Usage disk is at {percent}")
+    return percent  # Sempre restituisce percentuale, anche se < 80%
+
+def save_report():
+    with open("report.txt", "w") as f:
+        cpu = psutil.cpu_percent(interval=1)
+        ram = psutil.virtual_memory().percent
+        disk_usage = check_disk()
+        timestamp =  datetime.now().isoformat()
+        f.write(f"CPU usage: {cpu}%\n")
+        f.write(f"RAM usage: {ram}%\n")
+        f.write(f"Disk usage: {disk_usage}%\n")
+        f.write(f"timestamp : {timestamp}")
+
+
+# Chiamata della funzione per generare il report
+save_report()
